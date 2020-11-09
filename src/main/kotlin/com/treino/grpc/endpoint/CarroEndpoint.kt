0e00ef3eb85f9b16f10007cd3d0d66c4e42a14f6
Erick Alves <erickalvessud@gmail.com>
2020-11-09 11:44:07 -0300

    Cadastro de carro

--- src/main/kotlin/com/treino/grpc/endpoint/CarroEndpoint.kt
+++ src/main/kotlin/com/treino/grpc/endpoint/CarroEndpoint.kt
@@ -1,4 +1,19 @@
 package com.treino.grpc.endpoint
 
-class CarroEndpoint {
+import com.treino.*
+import com.treino.service.CarroService
+import javax.inject.Singleton
+
+@Singleton
+class CarroEndpoint(private val carroService: CarroService) : CarroServiceGrpcKt.CarroServiceCoroutineImplBase() {
+
+    @Override
+    override suspend fun consultaTodos(request: ConsultaTodosRequest): ConsultaTodosResponse {
+
+        return carroService.consultaTotos(request)!!
+    }
+
+    override suspend fun salvaCarro(request: SalvaCarroRequest): CarroReply {
+        return carroService.salvaCarro(request)
+    }
 }
--- src/main/kotlin/com/treino/service/CarroService.kt
+++ src/main/kotlin/com/treino/service/CarroService.kt
@@ -3,6 +3,8 @@ package com.treino.service
 import com.treino.CarroReply
 import com.treino.ConsultaTodosRequest
 import com.treino.ConsultaTodosResponse
+import com.treino.SalvaCarroRequest
+import com.treino.persistence.entities.CarroEntity
 import com.treino.persistence.repositories.CarroRepository
 import io.micronaut.data.model.Pageable
 import javax.inject.Singleton
@@ -10,7 +12,7 @@ import javax.inject.Singleton
 @Singleton
 class CarroService (private val carroRepository: CarroRepository){
 
-    public fun findAll(request: ConsultaTodosRequest): ConsultaTodosResponse.Builder? {
+    fun consultaTotos(request: ConsultaTodosRequest): ConsultaTodosResponse? {
         val carros = this.carroRepository.findAll(Pageable.from(request.page, request.size))
         val responseBuilder = ConsultaTodosResponse.newBuilder()
         carros.forEach { c ->
@@ -19,10 +21,40 @@ class CarroService (private val carroRepository: CarroRepository){
                             CarroReply.newBuilder()
                             .setId(c.id!!)
                             .setModelo(c.modelo)
+                            .setCor(c.cor)
+                            .setMarca(c.marca)
+                            .setPlaca(c.placa)
+                            .setValorDiaria(c.valorDiaria)
                             .build()
                     )
         }
 
-        return responseBuilder
+        responseBuilder.pageInfoBuilder
+                .setTotalPaginas(carros.totalPages.toLong())
+                .setPaginaAtual(carros.pageNumber.toLong())
+                .setTamanhoTotal(carros.totalSize)
+                .build()
+
+        return responseBuilder.build()
+    }
+
+    fun salvaCarro(request: SalvaCarroRequest): CarroReply {
+        val carroSalvo = this.carroRepository.save(CarroEntity(
+                null,
+                modelo = request.modelo,
+                cor = request.cor,
+                marca = request.marca,
+                placa = request.placa,
+                valorDiaria = request.valorDiaria
+        ))
+
+        return CarroReply.newBuilder()
+                .setId(carroSalvo.id!!)
+                .setModelo(carroSalvo.modelo)
+                .setCor(carroSalvo.cor)
+                .setMarca(carroSalvo.marca)
+                .setPlaca(carroSalvo.placa)
+                .setValorDiaria(carroSalvo.valorDiaria)
+                .build();
     }
 }
